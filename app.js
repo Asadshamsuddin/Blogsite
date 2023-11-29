184be151a87d1fa038db54c1644ea99bf9e56153
Muhammad Asad Shams Ud Din <143444162+Asadshamsuddin@users.noreply.github.com>
2023-11-29 14:04:44 +0500

    app.js

--- app.js
+++ app.js
@@ -11,27 +11,23 @@ const port = 8000;
 const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/blogsite');
+  await mongoose.connect('mongodb://127.0.0.1/blogsite');
   console.log('we are connected With Database')};
 
   // MONGOOSE SCHEMA
-const contactschema = new mongoose.Schema({
+const collectionSchema = new mongoose.Schema({
      name: String,
      email: String,
-     gender: String,
      password: String,
      address: String,
-     age: String,
-     locaction: String,
-     con: String,
+     age: Number,
+
    });
 
- const contact = mongoose.model('blogdata', contactschema);
-
+ const collection = mongoose.model('blogdata' , collectionSchema);
 // For Serving static files 
 app.use('/static' , express.static('static')); 
-app.use(express.urlencoded());
-
+app.use(express.urlencoded({extended:true}));
 
 //PUG SPEACIFIC STUFF/CUNFUGRATION
 app.set('view engine' , 'pug'); // Use template engine as a pug
@@ -54,14 +50,16 @@ app.get('/contact' , (req , res)=>{
      res.status(200).render('contact.pug');
 });
 
+
 app.post('/contact' , (req , res)=>{
-     var mydata = new contact(req.body);
+     var mydata = new collection(req.body);
      mydata.save().then(()=>{
           res.status(400).render('feedback.pug');
      }).catch(()=>{
           res.status(402).render('errorcontact.pug');
      });
 });
+
 
 app.get('/search' , (req , res)=>{
      res.status(200).render('search.pug');
@@ -71,39 +69,36 @@ app.get('/signup' , (req , res)=>{
      res.status(200).render('signup.pug');
 });
 
-app.post('/signup' , (req , res)=>{
-     var mydata = new contact(req.body);
-     mydata.save().then(()=>{
-       res.status(200).render('signup-sucessfully.pug');
-     }).catch(()=>{
-          res.status(200).render('failed-signup.pug');
-     });
+app.post('/signup' ,async (req , res)=>{
+
+     const data = {
+          email: req.body.email,
+           password: req.body.password
+      }
+       await collection.insertMany(data);
+      res.render("signin.pug");
+     
 });
 
-app.get('/signin' , (req , res)=>{
-     res.status(200).render('signin.pug');
-});
+app.get("/signin" , (req,res)=>{
+     res.render("signin.pug");
+})
+
 
 app.post('/signin' , async(req , res)=>{
-  
-      try {
-           const email =  req.body.email;
-           const password =  req.body.password;
-          //  const check = await blogdata.findOne({ email: req.body.email });
-  
-          if (check.password === req.body.password) {
-              res.status(202).render("home.pug");
-          }
-  
-          else {
-              res.send("Invalid User Name Or Password")
-          }
-      } 
-      
-      catch{
-          res.status(404).send(" <h1>User Not Found </h1>");
-      }
-  
+  try {
+     const check = await collection.findOne({email:req.body.email});
+     if(check.password===req.body.password){
+
+          res.render("home.pug")
+     }
+     else{
+         res.send("wrong password");
+     }
+  } catch (error) {
+     res.status(404).render("signinfail.pug");
+  }
+   
 });
 
 // Start the server
